b06b222cfd11335ae3bf6c224f61c1c2d3934b7f
Jimmy Jin <jimmyjin1119@gmail.com>
2025-01-03 16:37:06 -0800

    finding static files to pass into Active Learning class

--- ActiveLearningCycle.py
+++ ActiveLearningCycle.py
@@ -3,17 +3,18 @@ import numpy as np
 from sklearn.metrics import precision_recall_curve
 
 class ActiveLearningCycle:
-    def __init__(self, train_data, test_data, rounds, greedy_rounds=5, uncertain_rounds=5, save_path=""):
-        self.train_data = train_data
-        self.test_data = test_data
-        self.rounds = rounds
-        self.greedy_rounds = greedy_rounds
+    def __init__(self, input_csv, descriptors_csv, binders_csv, greedy_rounds=5, uncertain_rounds=5, output_directory=""):
+        self.input_csv = input_csv 
+        self.descriptors_csv = descriptors_csv
+        self.binders_csv = binders_csv
         self.uncertain_rounds = uncertain_rounds
-        self.save_path = save_path
+        self.greedy_rounds = greedy_rounds
+        self.output_directory = output_directory
 
-#run greedy rounds first to explore.
+#run uncertain rounds first to explore.
 
 #automate the removing of training data from the whole set
 
+#keep track of round number to dynamically name output files
 
     
--- main.py
+++ main.py
@@ -9,19 +9,24 @@ def main():
     selector.save_selected_batch(output_file='initial_batch.csv')
 
     # Step 2: Active learning loop
-    total_cycles = 10
     uncertain_cycles = 6
     greedy_cycles = 4
     output_dir = 'results/'
 
+    #random strategy called outside first to get docked, round 0
+    #random_strategy()
+
+    #files needed:
+    #'7nsw_all_hybrid.csv', docked_ecfp.csv, binders_docking.csv
     al_loop = ActiveLearningCycle(
-        initial_training_file='initial_batch.csv',
-        num_cycles=total_cycles,
+        '7nsw_all_hybrid.csv',
+        '../../../../docked_ecfp.csv',
+        '../../../../binders_docking.csv'
         num_uncertain=uncertain_cycles,
         num_greedy=greedy_cycles,
         output_dir=output_dir
     )
-    al_loop.run()
+
 
 if __name__ == '__main__':
     main()
